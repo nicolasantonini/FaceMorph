b5ec26deb16cead77f9b0f42f54b615d6b115395
Nicolas Antonini <n.antonini.consulente@alessandrocapra.com>
2022-02-01 18:31:30 +0100

    Disabled Landscape mode

--- app/src/main/java/it/unipr/advmobdev/mat301275/facemorph/MainActivity.java
+++ app/src/main/java/it/unipr/advmobdev/mat301275/facemorph/MainActivity.java
@@ -3,6 +3,7 @@ package it.unipr.advmobdev.mat301275.facemorph;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import org.opencv.android.OpenCVLoader;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MainActivity.this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
     }
 }
