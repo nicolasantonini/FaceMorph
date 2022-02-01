ba02927b42b998a7a0cb1ff38d662f4809ed46da
Nicolas Antonini <n.antonini.consulente@alessandrocapra.com>
2022-02-01 16:22:51 +0100

    OpenCV added to the project with static linking

--- app/src/main/java/it/unipr/advmobdev/mat301275/facemorph/MainActivity.java
+++ app/src/main/java/it/unipr/advmobdev/mat301275/facemorph/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
