758abee941ccbdb5b37b0a2c28cfa9944caa9bec
Nicolas Antonini <n.antonini.consulente@alessandrocapra.com>
2022-02-02 23:15:18 +0100

    Added automatic navigation in the Splash Module

--- app/src/main/java/it/unipr/advmobdev/mat301275/facemorph/modules/splash/SplashController.java
+++ app/src/main/java/it/unipr/advmobdev/mat301275/facemorph/modules/splash/SplashController.java
@@ -2,6 +2,8 @@ package it.unipr.advmobdev.mat301275.facemorph.modules.splash;
 
 import java.lang.ref.WeakReference;
 
+import it.unipr.advmobdev.mat301275.facemorph.authentication.AuthenticationManager;
+
 public class SplashController {
 
     private WeakReference<SplashFragment> weakFragment = null;
@@ -13,7 +15,12 @@ public class SplashController {
     public void viewDidLoad() {
         SplashFragment fragment = weakFragment.get();
         if (fragment != null) {
-            fragment.navigateToLogin();
+            if (AuthenticationManager.getInstance().isUserSignedIn()) {
+                fragment.navigateToHome();
+            } else {
+                fragment.navigateToLogin();
+            }
+
         }
     }
 
