4fe7aeb7029da54c3480667d6c6010ce3e36463d
Nicolas Antonini <n.antonini.consulente@alessandrocapra.com>
2022-02-03 00:15:38 +0100

    Created Home view

--- app/src/main/java/it/unipr/advmobdev/mat301275/facemorph/modules/home/HomeController.java
+++ app/src/main/java/it/unipr/advmobdev/mat301275/facemorph/modules/home/HomeController.java
@@ -1,8 +1,25 @@
 package it.unipr.advmobdev.mat301275.facemorph.modules.home;
 
+import java.lang.ref.WeakReference;
+
+
 public class HomeController {
 
-    public HomeController() {
+    private WeakReference<HomeFragment> weakFragment = null;
+
+    public HomeController(HomeFragment fragment) {
+        weakFragment = new WeakReference<>(fragment);
+    }
+
+    public void cameraPressed() {
+
+    }
+
+    public void settingsPressed() {
+
+    }
+
+    public void galleryPressed() {
 
     }
 
--- app/src/main/java/it/unipr/advmobdev/mat301275/facemorph/modules/home/HomeFragment.java
+++ app/src/main/java/it/unipr/advmobdev/mat301275/facemorph/modules/home/HomeFragment.java
@@ -2,14 +2,21 @@ package it.unipr.advmobdev.mat301275.facemorph.modules.home;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import it.unipr.advmobdev.mat301275.facemorph.R;
-import it.unipr.advmobdev.mat301275.facemorph.modules.login.LoginFragment;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,6 +24,12 @@ import it.unipr.advmobdev.mat301275.facemorph.modules.login.LoginFragment;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
+
+    HomeController controller = new HomeController(this);
+
+    private FloatingActionButton cameraButton = null;
+    private FloatingActionButton galleryButton = null;
+    private FloatingActionButton settingsButton = null;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -47,5 +60,34 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        cameraButton = (FloatingActionButton) getView().findViewById(R.id.camera_button);
+        galleryButton = (FloatingActionButton) getView().findViewById(R.id.gallery_button);
+        settingsButton = (FloatingActionButton) getView().findViewById(R.id.settings_button);
+
+        cameraButton.setOnClickListener(v -> controller.cameraPressed());
+        galleryButton.setOnClickListener(v -> controller.galleryPressed());
+        settingsButton.setOnClickListener(v -> controller.settingsPressed());
+    }
+
+    public void navigateToCamera() {
+        NavHostFragment.findNavController(this).navigate(R.id.action_homeFragment_to_cameraFragment);
+    }
+
+    public void navigateToSettings() {
+        NavHostFragment.findNavController(this).navigate(R.id.action_homeFragment_to_settingsFragment);
+    }
+
+    public void navigateToGallery() {
+        NavHostFragment.findNavController(this).navigate(R.id.action_homeFragment_to_galleryFragment);
+    }
+
+    public void navigateToPreview() {
+        NavHostFragment.findNavController(this).navigate(R.id.action_homeFragment_to_previewFragment);
     }
 }
